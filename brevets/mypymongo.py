13bacd484a16d1df83e2a84821dda9e55a33a39b
amontg14 <montgomery.austin00@gmail.com>
2023-03-01 23:02:11 -0800

    project 5 0.8
    code is working, need to make test

--- brevets/mypymongo.py
+++ brevets/mypymongo.py
@@ -5,10 +5,10 @@ client = MongoClient('mongodb://' + os.environ['MONGODB_HOSTNAME'], 27017)
 db = client.brevets
 collection = db.lists
 
-def brevet_insert(brevet_dist_km, start_time, items):
+def brevet_insert(brevet_dist_km, begin_date, items):
     output = collection.insert_one({
         "brevet_dist_km": brevet_dist_km,
-        "start_time": start_time,
+        "begin_date": begin_date,
         "items": items})
 
     _id = output.inserted_id # this is how you obtain the primary key (_id) mongo assigns to your inserted document.
@@ -17,4 +17,4 @@ def brevet_insert(brevet_dist_km, start_time, items):
 def brevet_find():
     lists = collection.find().sort("_id", -1).limit(1)
     for a_list in lists:
-        return a_list["brevet_dist"], a_list["start_time"], a_list["items"]
+        return a_list["brevet_dist_km"], a_list["begin_date"], a_list["items"]
